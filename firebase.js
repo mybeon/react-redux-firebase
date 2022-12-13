be4139d9b034617a959bca50e545b991f0e6f130
hichamelb <hichamelb54@gmail.com>
2022-12-13 14:14:59 +0100

    initialse firebase & firestore

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDPCbDdOxL-isye9fVyAXtMGcZqUYOMWA",
+  authDomain: "react-course-b2c0e.firebaseapp.com",
+  projectId: "react-course-b2c0e",
+  storageBucket: "react-course-b2c0e.appspot.com",
+  messagingSenderId: "624697145798",
+  appId: "1:624697145798:web:39578bcd588efee538dac7",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
