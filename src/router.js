a4047e65063d62f99c1bf700c95ba0fa6c9a193b
hichamelb <hichamelb54@gmail.com>
2022-12-21 17:41:37 +0100

    added terms page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Orders from "./pages/Orders";
+import Terms from "./pages/Terms";
 
 const router = createBrowserRouter([
   {
@@ -8,6 +9,7 @@ const router = createBrowserRouter([
     element: <Home />,
   },
   { path: "/orders", element: <Orders /> },
+  { path: "/terms", element: <Terms /> },
 ]);
 
 export default router;
